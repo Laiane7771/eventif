d55bb21a2b6a3ebc93859b4e258c27372425eab0
laiane <llaiane_lindinha@hotmail.com>
2015-05-21 11:34:51 -0300

    primeiro commit

---
+++ EventIf/src/Principal/Main.java
@@ -0,0 +1,12 @@
+package Principal;
+import br.edu.IFG.TADS.eventif.controll.TelaInicial;
+
+public class Main {
+	
+	public static void main(String [] args){
+		
+		TelaInicial t = new TelaInicial();
+		t.telaInicial();
+	}
+
+}
---
+++ EventIf/src/br/edu/IFG/TADS/eventif/controll/TelaInicial.java
@@ -0,0 +1,22 @@
+package br.edu.IFG.TADS.eventif.controll;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class TelaInicial {
+
+	
+	public void telaInicial(){
+		
+		JFrame tela = new JFrame("Home");
+		tela.setSize(1024, 800);
+		
+		JPanel tela1 = new JPanel();
+		tela.add(tela1);
+		
+		
+	}
+
+	
+
+}
